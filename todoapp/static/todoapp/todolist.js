e9f476ec2ae6dcd1c02dd39a5e5093a60bcaf6e8
John Carlo <johncarlosabenorio07@gmail.com>
2024-03-06 17:58:22 +0800

    added draggables - 1

--- todoapp/static/todoapp/todolist.js
+++ todoapp/static/todoapp/todolist.js
@@ -1,6 +1,13 @@
 addBtn = document.getElementById('add-task');
+draggables = document.querySelectorAll('.draggable');
+taskDiv = document.querySelector('.task-div');
 
 
+draggables.forEach((draggable) => {
+    draggable.addEventLIstener('dragstart', () =>{
+
+    });
+});
 
 
 
