511852ccab2a02b8b7780ee7f89140890ae91f87
John Carlo <johncarlosabenorio07@gmail.com>
2024-03-13 20:41:58 +0800

    add draggables

--- todoapp/static/todoapp/todolist.js
+++ todoapp/static/todoapp/todolist.js
@@ -1,17 +1,58 @@
-addBtn = document.getElementById('add-task');
-draggables = document.querySelectorAll('.draggable');
-taskDiv = document.querySelector('.task-div');
+const addBtn = document.getElementById('add-task');
+let draggables = document.querySelectorAll('.draggable');
+const taskContainer = document.querySelector('.task-div');
 
 
-draggables.forEach((draggable) => {
-    draggable.addEventLIstener('dragstart', () =>{
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () =>{
+        draggable.classList.add('dragging');
+    });
 
+    draggable.addEventListener('dragend', ()=> {
+        draggables = document.querySelectorAll('.draggable');
+        draggable.classList.remove("dragging");
+        updateTaskOrder(draggables);
+    });
+
+    taskContainer.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        const afterElement = getDragAfterElement(taskContainer, e.clientY);
+        const dragTask = document.querySelector('.dragging');
+        if (afterElement == null){
+            taskContainer.appendChild(dragTask);
+        }
+        else{
+            taskContainer.insertBefore(dragTask, afterElement);
+        }
     });
 });
 
 
 
-
 function addTask(){
     alert("Adding a task!");
+}
+
+function updateTaskOrder(tasks){
+    console.log(tasks);
+
+    let taskCount = 1;
+    for(let i = 0; i < tasks.length; i++){
+        tasks[i].id = 'task-' + taskCount;
+        taskCount++;
+    }
+}
+
+function getDragAfterElement(container, y){
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
+    return draggableElements.reduce((closest, child)=>{
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset){
+            return { offset: offset, element: child }
+        }
+        else{
+            return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
 }
