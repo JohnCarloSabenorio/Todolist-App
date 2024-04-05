6add08ad8f2c2a777cb33f74cc4e750ec7eb879b
John Carlo Sabenorio <140798194+JohnCarloSabenorio@users.noreply.github.com>
2024-04-05 13:49:01 +0800

    remove include from import

--- todoproject/urls.py
+++ todoproject/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from django.contrib.auth import views as auth_views
 from todoapp import views as user_views
 
